cfdeadf5fb1e7cf7d5fc61a3b444c7ad1123058c
vipulgarg1998 <garg.vipul7@gmail.com>
2023-01-09 11:55:00 +0000

    download weights using bash file

---
+++ weights/download_weights.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download the pose weights
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-w6-person.pt
+
+# Download the instance segmentation weights
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-mask.pt
